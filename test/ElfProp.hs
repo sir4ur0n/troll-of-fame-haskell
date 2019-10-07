25ccaf87d6fd71188dbb04855ded0d7617b86778
Julien Debon <julien.debon@decathlon.com>
2019-10-07 15:15:21 +0200

    Cleanup

--- test/ElfProp.hs
+++ test/ElfProp.hs
@@ -1,11 +1,11 @@
 module ElfProp where
-
-import           Elf
-import           Generators            ()
-import           Test.Tasty.QuickCheck
-
-test_elfValueProductRaceRole =
-  testProperty "The value of a High elf must be an even number" $ \elf ->
-    if race elf == High
-      then value elf `mod` 2 == 0
-      else True
+--
+--import           Elf
+--import           Generators            ()
+--import           Test.Tasty.QuickCheck
+--
+--test_elfValueProductRaceRole =
+--  testProperty "The value of a High elf must be an even number" $ \elf ->
+--    if race elf == High
+--      then value elf `mod` 2 == 0
+--      else True
